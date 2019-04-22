5fe3615e9c64f65859f8e078bc768a5a5a28cdf8
Parrot <parrot@localhost.localdomain>
2019-04-22 21:15:10 +0200

    Deleted the password from app configuration

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "flask.microblog.246"
-app.config['MAIL_PASSWORD'] = "flask_microblog246"
+app.config['MAIL_PASSWORD'] = "#"
 mail = Mail(app)
 
 from flaskapp import routes
